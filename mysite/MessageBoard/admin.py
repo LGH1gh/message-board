39ae3714a385f60c5ecdaf6bdd259e16a4e7e814
LGH1gh <jokerliveswzy@gmail.com>
2018-08-03 16:57:48 +0800

    注册数据库

--- mysite/MessageBoard/admin.py
+++ mysite/MessageBoard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import User
+from .models import Board
+from .models import Agree
+from .models import Comment
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Board)
+admin.site.register(Agree)
+admin.site.register(Comment)
